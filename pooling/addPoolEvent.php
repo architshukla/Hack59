963c72fc138e41875721a78afb63f36623ff45de
sandeeprv <r.v_sandeep@yahoo.co.in>
2014-03-21 03:30:27 +0530

    made pool events

--- pooling/addPoolEvent.php
+++ pooling/addPoolEvent.php
@@ -4,7 +4,7 @@
 	$amount = $_POST['amount'];
 
 	require_once("../includes/connection.php");
-	if(mysqli_query($connection, "insert into poolevents(eventname, creatorname, amount) values(\"$name\",\"$creatorname\", $amount);") == FALSE)
+	if(mysqli_query($connection, "insert into poolevents(eventname, creatorname,settled, amount) values(\"$name\",\"$creatorname\",0, $amount);") == FALSE)
 	{
 		echo "error:Pool event creation failed. ".mysqli_error($connection);
 	}
