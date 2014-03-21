776175d9232ea56913de110d93e20e2abb7c931e
sandeeprv <r.v_sandeep@yahoo.co.in>
2014-03-21 06:27:39 +0530

    finished pool

--- pooling/getPledgeList.php
+++ pooling/getPledgeList.php
@@ -1,20 +1,28 @@
 <?php
-	$id = 1;
-
-
+	$eventid = $_POST['eventid'];
 	require_once("../includes/connection.php");
 	$jsonArray = [];
-	if(($data = mysqli_query($connection, "select * from poolmembers where eventid=$id")) == FALSE)
+	if(($data = mysqli_query($connection, "select * from poolevents where eventid=$eventid")) == FALSE)
 	{
 		echo "error:Fetch failed. ".mysqli_error($connection);
+	} else {
+		$poolevent = mysqli_fetch_array($data);
+		if ($poolevent["settled"]==1) {
+			if(($data = mysqli_query($connection, "select * from poolmembers where eventid=$eventid")) == FALSE)
+			{
+				echo "error:Fetch failed. ".mysqli_error($connection);
+			}
+			else
+			{
+				while($pledger = mysqli_fetch_array($data))
+				{
+					array_push($jsonArray, array("name"=>$pledger["membername"], "amount"=> $pledger["amount"]));
+				}		
+			}
+			echo json_encode($jsonArray);
+		}else{
+			echo "message: pool event is not yet settled to view the list";
+		}
 	}
-	else
-	{
-		while($pledger = mysqli_fetch_array($data))
-		{
-			array_push($jsonArray, array("name"=>$pledger["membername"], "amount"=> $pledger["amount"]));
-		}		
-	}
-	echo json_encode($jsonArray);
 	require_once("../includes/footer.php");
 ?>
--- pooling/pledgeAmount.php
+++ pooling/pledgeAmount.php
@@ -1,10 +1,10 @@
 <?php
-	$id = 1;
-	$name = "rohit";
-	$amount = 1000;
+	$eventid = $_POST['eventid'];
+	$name = $_POST['name'];
+	$amount = $_POST['amount'];
 
 	require_once("../includes/connection.php");
-	if(($data = mysqli_query($connection, "select * from poolevents where eventid=$id")) == FALSE)
+	if(($data = mysqli_query($connection, "select * from poolevents where eventid=$eventid")) == FALSE)
 	{
 		echo "error:Event not found. ".mysqli_error($connection);
 	}
@@ -16,7 +16,7 @@
 		return;
 	}
 
-	if(mysqli_query($connection, "update poolmembers set amount=amount+$amount where eventid=$id and membername='$name'") == FALSE)
+	if(mysqli_query($connection, "update poolmembers set amount=amount+$amount where eventid=$eventid and membername='$name'") == FALSE)
 	{
 		echo "error:Pledging failed. ".mysqli_error($connection);
 	}
@@ -25,14 +25,14 @@
 		echo "success:Pledged successfully!";
 	}
 
-	if(($data = mysqli_query($connection, "select sum(amount) from poolmembers where eventid=$id")) == FALSE)
+	if(($data = mysqli_query($connection, "select sum(amount) from poolmembers where eventid=$eventid")) == FALSE)
 	{
 		echo "error:Event not found. ".mysqli_error($connection);
 	}
 	$sum = mysqli_fetch_array($data);
 	if($sum[0] >= $event["amount"])
 	{
-		if(mysqli_query($connection, "update poolevents set settled=1 where eventid=$id") == FALSE)
+		if(mysqli_query($connection, "update poolevents set settled=1 where eventid=$eventid") == FALSE)
 		{
 			echo "error:Pledging failed. ".mysqli_error($connection);
 		}
