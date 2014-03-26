dd148573ecb996ac1b5ae207ff3eba47d086d406
sandeeprv <r.v_sandeep@yahoo.co.in>
2014-03-26 20:05:38 +0530

    sandeep changes

--- yodlee/credentials.php
+++ yodlee/credentials.php
@@ -1,14 +1,8 @@
 <?php
 	session_start();
-	$filehandle = fopen("credentials.txt", "r");
-	while(!feof($filehandle))
-	{
-		$string = fgets($filehandle);
-		if(isset($_SESSION['username']))
-			$array = array("login"=> trim($_SESSION['username']), "password"=> trim($string));
-		else
-			$array = array("login"=> trim('archit'), "password"=> trim($string));
-	}
+	
+		$array = array("login"=> trim('sbMemshashank1'), "password"=> trim("sbMemshashank1#123"));
+	
 	header('Content-type: application/json');
 	echo json_encode($array);
 	fclose($filehandle);
